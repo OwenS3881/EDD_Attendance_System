574c479f62e741a514ef817964f9c34629989319
OwenS3881 <owenski08@gmail.com>
2024-03-08 15:23:18 -0500

    A3

--- EDD_Attendance_System/Assets/Scripts/StudentExcuseRequestManager.cs
+++ EDD_Attendance_System/Assets/Scripts/StudentExcuseRequestManager.cs
@@ -244,7 +244,7 @@ public class StudentExcuseRequestManager : MonoBehaviour
         Database.instance.SaveDataToStorage(pic);
         Debug.Log("Post Call");
         */
-        Database.instance.PutImage("https://firebasestorage.googleapis.com/v0/b/edd-attendance.appspot.com/o/", bytes);
+        Database.instance.PutImage("https://firebasestorage.googleapis.com/v0/b/edd-attendance.appspot.com/o/test1", bytes);
     }
 
     private void UpdateCameraRenderer()
